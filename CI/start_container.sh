9ac2e082f3b2b1f49b8556fd013dd96e1f72ae96
Ajith Kumar <ajithkumargupi@gmail.com>
2025-09-03 10:02:44 +0530

    Update start_container.sh

--- CI/start_container.sh
+++ CI/start_container.sh
@@ -4,6 +4,7 @@ set -e
 # Pull the Docker image from Docker Hub
 docker pull ajith1610/simple-python-flask-app
 
+
 # Run the Docker image as a container
 docker run -d -p 5000:5000 ajith1610/simple-python-flask-app
 
